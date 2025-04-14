2073d8b0b2b83ffd38e46d0f356aa52cf2487c4a
Alex Belov <alex@cornel.su>
2025-04-15 00:11:36 +0300

    Initial commit with codemagic.yaml

--- Buffalo Gaming/Buffalo_GamingApp.swift
+++ Buffalo Gaming/Buffalo_GamingApp.swift
@@ -1,17 +1,10 @@
-//
-//  Buffalo_GamingApp.swift
-//  Buffalo Gaming
-//
-//  Created by alex on 4/14/25.
-//
-
 import SwiftUI
 
 @main
 struct Buffalo_GamingApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            BuffaloMainGameScreen()
         }
     }
 }
