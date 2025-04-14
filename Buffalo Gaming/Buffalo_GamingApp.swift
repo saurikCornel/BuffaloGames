592bf2065ccb7fe625f71a071edb193c93bff110
Alex Belov <alex@cornel.su>
2025-04-14 22:22:57 +0300

    Initial Commit

---
+++ Buffalo Gaming/Buffalo_GamingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Buffalo_GamingApp.swift
+//  Buffalo Gaming
+//
+//  Created by alex on 4/14/25.
+//
+
+import SwiftUI
+
+@main
+struct Buffalo_GamingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
